531423d6364ddec51ebe83e1ab3b99826b1c2377
Ivan <ivan@yandex.ru>
2019-01-12 01:32:38 +0300

    Go

--- ConsoleApp1/Module1.vb
+++ ConsoleApp1/Module1.vb
@@ -1,7 +1,28 @@
-﻿Module Module1
+﻿Imports System.Threading.Tasks
+Imports System.Threading
+'как понять Где начинается callback и что происходит с вызывающим потоком
+
+Module Module1
 
 	Sub Main()
+		'
+		mySubAsync(10) ' запускается Task -> т.е. параллельная работа -) в консоле!
+		mySub(20) 'consoleEND 5 (из параллели пришел callback)
+		Console.ReadLine()
+	End Sub
+	Private Sub mySub(ByVal x As Integer)
+		'x = x * 10
+		For i = 0 To x
+			'Thread.Sleep(500)
+			Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} ...i={i}")
+		Next
+	End Sub
+	Private Async Sub mySubAsync(ByVal x As Integer)
+		mySub(x)
+		Await Task.Run(Sub()
+						   mySub(x)
+					   End Sub)
+		Console.WriteLine("END>>>")
 
 	End Sub
-
 End Module
